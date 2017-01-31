aa4126f1d5e8a1ce1c8497c09196024935e5c347
cpw8754 <cpw8754@louisiana.edu>
2017-01-31 00:13:58 -0600

    Project up to creating collectables.
    Here are all the files for the CMPS 499 Unity assignment so far.

---
+++ CMPS_499_Roll_a_Ball_cpw8754/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private bool inAir = false;
+
+    // Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        //rb.freezeRotation = true;
+	}
+	
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (rb.transform.position.y <= 0.75)
+        {
+            inAir = false;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+        if (!inAir && Input.GetKeyUp(KeyCode.Space))
+        {
+            movement = new Vector3(moveHorizontal, jump, moveVertical);
+            inAir = true;
+        }
+
+        rb.AddForce(movement * speed);
+
+
+    }
+}
